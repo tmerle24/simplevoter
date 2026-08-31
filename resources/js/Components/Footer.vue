375b78548ea61f49aadf4102fbd5a11da9c6caee
tmerle <t.merle.extern@macrocom.de>
2026-08-31 10:41:25 +0200

    SolvedApps-Badge im Footer auf doppelte Größe bringen

--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -37,7 +37,7 @@ const props = defineProps({
         rel="noopener"
         aria-label="solvedapps"
         class="opacity-50 transition-opacity hover:opacity-90"
-      ><img src="/images/solvedapps-wordmark.svg" alt="solvedapps" width="65" height="16" /></a>
+      ><img src="/images/solvedapps-wordmark.svg" alt="solvedapps" width="130" height="32" /></a>
     </div>
   </footer>
 </template>
